a97435a1195a31c4d44dfb584ed10b8d9c7a3eb6
Ndibechinonso <edwardndibe@gmail.com>
2022-07-01 16:13:54 +0100

    hero add

--- components/Homepage/HeroSection.js
+++ components/Homepage/HeroSection.js
@@ -1,8 +1,13 @@
+import Image from "next/image"
 import styles from "../../styles/Home.module.scss"
 const HeroSection = () => {
     return(
-        <div className={`${styles.hero} bg-hero_img bg-no-repeat h-[90vh] w-[100%]`}>
-<h2>HERO</h2>
+        <div className={`${styles.hero} relative bg-hero_img bg-no-repeat h-[90vh] w-[100%]`}>
+       <div className="absolute top-[3.707rem]"><Image src="/images/leftCircle.svg" width={527.21} height={527.21} /></div>
+       <div className="absolute left-[28.205%]"><Image src="/images/middleCircle.svg" width={536.48} height={536.48} /></div>
+
+       <div className="absolute right-0"><Image src="/images/rightCircle.svg" width={484.07} height={484.07} /></div>
+
         </div>
     )
 }
