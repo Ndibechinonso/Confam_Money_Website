a1eec52ad61a0343204b954f95de371c42712a21
Ndibechinonso <edwardndibe@gmail.com>
2022-10-04 09:16:25 +0100

    contact corr

--- pages/scam_control.js
+++ pages/scam_control.js
@@ -22,8 +22,8 @@ const ScamControl = () => {
       />
 <ScamControlGrid />
 <h3 className="text-center font-semibold text-[1.125rem] md:text-[2rem] text-grey_1 mt-[4rem] md:mt-[10.5rem] mb-[0.875rem] md:mb-[1.5rem]">Report suspicious transactions to us</h3>
-<p className="mr-1 m-auto text-center max-w-[322px] px-[1.125rem] md:max-w-[900px] leading-[1.5rem] md:leading-[1.875rem] text-grey_2 text-[1rem] md:text-[1.25rem] mb-[9.375rem] md:mb-[14.25rem]">We can be reached directly via our customer service lines, you can also send us a mail for prompt 
-mediation and resolution.<span><Link href="/contact"><a className="text-scam_contact font-medium">Contact.</a></Link></span></p>
+<p className="m-auto text-center max-w-[322px] px-[1.125rem] md:max-w-[900px] leading-[1.5rem] md:leading-[1.875rem] text-grey_2 text-[1rem] md:text-[1.25rem] mb-[9.375rem] md:mb-[14.25rem]">We can be reached directly via our customer service lines, you can also send us a mail for prompt 
+mediation and resolution.<span className="ml-1"><Link href="/contact"><a className="text-scam_contact font-medium">Contact.</a></Link></span></p>
 </main>
         </>
     )
